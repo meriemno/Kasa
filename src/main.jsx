2d3d1275511fe4ac46e96d336a63e8fc2826a959
NORI Meriem <meriem.jebbour@gmail.com>
2024-05-07 22:56:04 +0200

    page error :desktop

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import About from './pages/About.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <Home /> */}
-    {/* <Error /> */}
-    <About/>
+    <Error />
+    {/* <About/> */}
   </React.StrictMode>,
 )
